f48e5288b94151ad18f9509c05e7e96470892210
Leandro Ross <leandroross@gmail.com>
2017-02-27 12:12:47 -0300

    change database recreation to console command

--- AuthServer/mainProgram.cs
+++ AuthServer/mainProgram.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Reflection;
+using Common.Database;
 using Common.Globals;
 using Common.Helpers;
 
@@ -25,14 +26,10 @@ namespace AuthServer
 
             Database = new AuthServerDatabase();
 
-            // recreate database***
-            //new DatabaseManager();
-
             //
             AuthServerRouter.AddHandler<AuthLogonChallenge>(AuthCMD.CMD_AUTH_LOGON_CHALLENGE, AuthServerHandler.OnAuthLogonChallenge);
             AuthServerRouter.AddHandler<AuthLogonProof>(AuthCMD.CMD_AUTH_LOGON_PROOF, AuthServerHandler.OnAuthLogonProof);
             AuthServerRouter.AddHandler(AuthCMD.CMD_AUTH_REALMLIST, AuthServerHandler.OnAuthRealmList);
-
 
             Log.Print(LogType.AuthServer,
                 $"Successfully started in {Time.GetMsTimeDiff(time, Time.GetMsTime()) / 1000}s");
@@ -43,6 +40,11 @@ namespace AuthServer
                 var command = Console.ReadLine();
                 switch (command)
                 {
+                    case "/db":
+                        new DatabaseManager();
+                        Log.Print(LogType.Console, $"Database recreated.");
+                        break;
+
                     case "/gc":
                         GC.Collect();
                         Log.Print(LogType.Console,
@@ -54,7 +56,7 @@ namespace AuthServer
                         break;
 
                     default:
-                        Log.Print(LogType.Console, $"Unknown Command: {command}");
+                        Log.Print(LogType.Debug, $"Unknown Command: {command}");
                         break;
                 }
             }
